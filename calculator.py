8253248f4256dadd7dacd538dec80c8cd6aa186c
romanusbett <bettromanus@gmail.com>
2025-08-01 13:59:51 +0300

    add calculator file

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input('enter the first number'))
+num2 = int(input('enter the second number'))
+
+def add(n1, n2):
+    print(n1 + n2)
+
+def subtract(n1, n2):
+    print(n1 - n2)
+    
+def divide(n1, n2):
+    print(n1 / n2)
+
+def multiplication(n1, n2):
+    print(n1 * n2)
+    
+add(num1, num2)
+subtract(num1, num2)
+divide(num1, num2)
+multiplication(num1, num2)
